af25d5a1d7ec7a2ff27f5d05ec9ecdc0a3e47a65
Zimuzo Akanne <127262688+Zim67@users.noreply.github.com>
2024-03-19 01:02:16 -0230

    Add files via upload

--- dal/database.js
+++ dal/database.js
@@ -3,7 +3,7 @@ const { Pool } = require("pg");
 const pool = new Pool({
   user: "postgres",
   host: "localhost",
-  database: "PenneyC_QAP3",
+  database: "Zim67FS-DB-QAP3--ED",
   password: "admin", // Make sure to provide the correct password here
   port: 5432,
 });
